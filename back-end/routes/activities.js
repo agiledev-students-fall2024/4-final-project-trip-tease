ba1dd436b92f89bcaa6986dad7b10a5a841c1504
akana3866 <akanaujia8@gmail.com>
2024-12-03 23:16:04 -0500

    Added activities and location validators

--- back-end/routes/activities.js
+++ back-end/routes/activities.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import activitiesController from '../controllers/activitiesController.js';
-import { ensureAuthenticated } from '../config/jwt-config.js'; // Middleware for authentication
+import { ensureAuthenticated } from '../config/jwt-config.js'; 
+import { validateGetActivityById, validateCreateActivity, validateDeleteComment, validateCreateComment, validateGetActivitiesByLocationId, validateUpvoteActivity, validateDownvoteActivity } from '../validators/activitiesValidators.js';
 
 
 const router = express.Router();
@@ -9,24 +10,24 @@ const router = express.Router();
 router.get('/', ensureAuthenticated, activitiesController.getActivities);
 
 // Get activities by location ID
-router.get('/location/:locationId', ensureAuthenticated, activitiesController.getActivitiesByLocation);
+router.get('/location/:locationId', ensureAuthenticated, validateGetActivitiesByLocationId, activitiesController.getActivitiesByLocation);
 
 // Get a specific activity by ID
-router.get('/:activityId', ensureAuthenticated, activitiesController.getActivityById);
+router.get('/:activityId', ensureAuthenticated, validateGetActivityById, activitiesController.getActivityById);
 
 // Create a new activity
-router.post('/', ensureAuthenticated, activitiesController.createActivity);
+router.post('/', ensureAuthenticated, validateCreateActivity, activitiesController.createActivity);
 
 // Upvote an activity
-router.post('/:activityId/upvote', ensureAuthenticated, activitiesController.upvoteActivity);
+router.post('/:activityId/upvote', ensureAuthenticated, validateUpvoteActivity, activitiesController.upvoteActivity);
 
 // Downvote an activity
-router.post('/:activityId/downvote', ensureAuthenticated, activitiesController.downvoteActivity);
+router.post('/:activityId/downvote', ensureAuthenticated, validateDownvoteActivity, activitiesController.downvoteActivity);
 
 // Add a comment to an activity
-router.post('/:activityId/comments', ensureAuthenticated, activitiesController.addCommentToActivity);
+router.post('/:activityId/comments', ensureAuthenticated, validateCreateComment, activitiesController.addCommentToActivity);
 
 // Delete a comment from an activity
-router.delete('/:activityId/comments/:commentId', ensureAuthenticated, activitiesController.deleteCommentFromActivity);
+router.delete('/:activityId/comments/:commentId', ensureAuthenticated, validateDeleteComment, activitiesController.deleteCommentFromActivity);
 
 export default router;
---
+++ back-end/validators/activitiesValidators.js
@@ -0,0 +1,61 @@
+import { param, body } from 'express-validator';
+import { validate } from './validatorsCommon.js';
+
+
+export const validateGetActivityById = validate([
+    param('activityId').isMongoId().withMessage('Invalid Activity ID, it must be a valid MongoDB ObjectId.')
+]);
+
+export const validateCreateActivity = validate([
+    body('name').notEmpty().withMessage('Activity name is required.'),
+    body('locationId').notEmpty().isMongoId().withMessage('Location ID must be a valid MongoDB ObjectId.'),
+    body('type')
+        .notEmpty()
+        .isIn(['food', 'activities', 'stay'])
+        .withMessage('Type must be one of: food, activities, stay.'),
+    body('price').if((value) => value !== undefined).isInt({ min: 0, max: 4 }).withMessage('Price must be an integer between 0 and 4.'),
+    body('description').if((value) => value !== undefined && value !== '').isString().withMessage('Description must be a string.'),
+    body('image').if((value) => value !== undefined && value !== '').isURL().withMessage('Image must be a valid URL.'),
+]);
+
+export const validateCreateComment = validate([
+    param('activityId')
+        .isMongoId()
+        .withMessage('Invalid Activity ID, it must be a valid MongoDB ObjectId.'),
+    body('userId')
+        .notEmpty()
+        .isMongoId()
+        .withMessage('User ID is required and must be a valid MongoDB ObjectId.'),
+    body('commentString')
+        .notEmpty()
+        .isString()
+        .withMessage('Comment content is required and must be a string.')
+]);
+
+export const validateDeleteComment = validate([
+    param('activityId')
+        .isMongoId()
+        .withMessage('Invalid Activity ID, it must be a valid MongoDB ObjectId.'),
+    param('commentId')
+        .isString()
+        .notEmpty()
+        .withMessage('Comment ID is required and must be a valid string.')
+]);
+
+export const validateGetActivitiesByLocationId = validate([
+    param('locationId')
+        .isMongoId()
+        .withMessage('Invalid Location ID, it must be a valid MongoDB ObjectId.')
+]);
+
+export const validateUpvoteActivity = validate([
+    param('activityId')
+        .isMongoId()
+        .withMessage('Invalid Activity ID, it must be a valid MongoDB ObjectId.')
+]);
+
+export const validateDownvoteActivity = validate([
+    param('activityId')
+        .isMongoId()
+        .withMessage('Invalid Activity ID, it must be a valid MongoDB ObjectId.')
+]);
